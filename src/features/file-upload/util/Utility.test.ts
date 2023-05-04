953b844811a8c936f3a024fb5a7b08851d82ab2b
Usman <usman.aslam@yicom.com>
2023-05-03 22:48:57 -0400

    mocking delay

--- src/features/file-upload/util/Utility.test.ts
+++ src/features/file-upload/util/Utility.test.ts
@@ -22,31 +22,34 @@ Additional aspects:
 - The function uses Constants.MockUploadTime to mock the upload time for each file.
 */
 
-
 import { completeProgress } from "./Utility";
+jest.mock("./constants", () => ({
+  ...jest.requireActual("./constants"),
+  MockUploadTime: 10,
+}));
 
-describe('completeProgress_function', () => {
-    // Tests that the function successfully sets the upload percentage for the given number of files. 
-    it("test_complete_progress_with_valid_input", async () => {
-        const setUploadPercent = jest.fn();
-        const numberOfFiles = 5;
-        await completeProgress(numberOfFiles, setUploadPercent);
-        expect(setUploadPercent).toHaveBeenCalledTimes(numberOfFiles * 10);
-    });
+describe("completeProgress_function", () => {
+  // Tests that the function successfully sets the upload percentage for the given number of files.
+  it("test_complete_progress_with_valid_input", async () => {
+    const setUploadPercent = jest.fn();
+    const numberOfFiles = 5;
+    await completeProgress(numberOfFiles, setUploadPercent);
+    expect(setUploadPercent).toHaveBeenCalledTimes(numberOfFiles * 10);
+  });
 
-    // Tests that the promise resolves with 100 when the upload is complete. 
-    it("test_complete_progress_resolves_with_100", async () => {
-        const setUploadPercent = jest.fn();
-        const numberOfFiles = 5;
-        const result = await completeProgress(numberOfFiles, setUploadPercent);
-        expect(result).toBe(100);
-    });
+  // Tests that the promise resolves with 100 when the upload is complete.
+  it("test_complete_progress_resolves_with_100", async () => {
+    const setUploadPercent = jest.fn();
+    const numberOfFiles = 5;
+    const result = await completeProgress(numberOfFiles, setUploadPercent);
+    expect(result).toBe(100);
+  });
 
-    // Tests that the function handles the case where numberOfFiles is 0. 
-    it("test_complete_progress_with_zero_files", async () => {
-        const setUploadPercent = jest.fn();
-        const numberOfFiles = 0;
-        const result = await completeProgress(numberOfFiles, setUploadPercent);
-        expect(result).toBe(100);
-    });
+  // Tests that the function handles the case where numberOfFiles is 0.
+  it("test_complete_progress_with_zero_files", async () => {
+    const setUploadPercent = jest.fn();
+    const numberOfFiles = 0;
+    const result = await completeProgress(numberOfFiles, setUploadPercent);
+    expect(result).toBe(100);
+  });
 });
